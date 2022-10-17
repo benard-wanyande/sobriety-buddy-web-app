62ff47ca508b751ef5258b0c046eb241185d1c69
Adala Wanyande <awanyande@gmail.com>
2022-10-18 02:15:27 +0300

    Add Tailwind CSS and simple splashscreen using Livewire

---
+++ resources/views/livewire/splashscreen.blade.php
@@ -0,0 +1,16 @@
+<div wire:click="toLoginPage" >
+    {{-- Stop trying to control. --}}  
+    <div class="bg-gradient-to-r from-green-400 to-blue-500 grid place-content-center place-items-center h-screen w-screen" >
+        {{-- Background wallpaper --}}
+        <div class="animate-pulse bg-white text-neutral-400 h-16 w-16 grid place-content-center place-items-center rounded mb-8 text-2xl font-bold">
+            {{-- Sobriety buddy logo --}}
+            SB
+        </div>
+        <div>
+            {{-- Sobriety buddy text --}}
+            <h1 class="animate-pulse text-xl font-bold text-white">
+                SOBRIETY BUDDY
+            </h1>
+        </div>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/splashscreen', function () {
+    return view('livewire.splashscreen');
+});
+
+Route::get('/login', function () {
+    return view('livewire.login');
+});
